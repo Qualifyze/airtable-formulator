83a8fe328c48ba3f98d2eeba41950f500e18c72d
Garik Suess <garik.suess@qualifyze.com>
2022-09-14 15:24:53 +0200

    Adds AST Parsing lib

---
+++ src/ast/nodes/modifier.ts
@@ -0,0 +1,118 @@
+import {
+  createGroup,
+  createNodeErrorMessage,
+  filterMeaningfulNodes,
+  GroupNode,
+  isGroupNode,
+  isNode,
+  isOperator,
+  isSpace,
+  isUnaryOperator,
+  Node,
+  Operator,
+  Space,
+  UnaryOperator,
+} from "./primitives";
+import { ExpressionNode, isExpressionNode } from "./expression";
+
+export interface ModifierNode
+  extends GroupNode<"modifier", ModifierNodeMember> {
+  readonly operator: Operator;
+  readonly operand: ExpressionNode;
+}
+
+type ModifierNodeMember = Space | UnaryOperator | ExpressionNode;
+
+function isModifierNodeMember(node: Node): node is ModifierNodeMember {
+  return isSpace(node) || isUnaryOperator(node) || isExpressionNode(node);
+}
+
+export function isModifierNode(node: Node): node is ModifierNode {
+  const { operator, operand } = node as unknown as Record<string, unknown>;
+
+  return (
+    isGroupNode(node) &&
+    node.members.every(isModifierNodeMember) &&
+    node.type === "modifier" &&
+    isNode(operator) &&
+    isUnaryOperator(operator) &&
+    isNode(operand) &&
+    isExpressionNode(operand)
+  );
+}
+
+function createModifier(nodes: ModifierNodeMember[]): ModifierNode {
+  const modifier = createGroup("modifier", nodes);
+  const [operator, operand, invalid] = filterMeaningfulNodes(nodes);
+
+  if (!operator || !isOperator(operator)) {
+    throw new Error(
+      createNodeErrorMessage(
+        modifier,
+        `Modifier must have an operator, but encountered ${operator.type} instead`
+      )
+    );
+  }
+  if (!operand || !isExpressionNode(operand)) {
+    throw new Error(
+      createNodeErrorMessage(
+        modifier,
+        `Modifier must have an operand expression, but encountered ${operand.type} instead`
+      )
+    );
+  }
+  if (invalid) {
+    throw new Error(
+      createNodeErrorMessage(
+        modifier,
+        `Modifier must have exactly 2 members, but encountered an extra ${invalid.type} instead`
+      )
+    );
+  }
+
+  return {
+    ...modifier,
+    operator,
+    operand,
+  };
+}
+
+export const reduceModifiers = ([...nodes]: readonly Node[]): Node[] => {
+  const meaningfulNodes = filterMeaningfulNodes(nodes);
+
+  const operators: Operator[] = meaningfulNodes.filter(
+    isUnaryOperator
+  ) as Operator[];
+
+  const nodeGroups: ModifierNodeMember[][] = operators
+    .map((operator) => {
+      const operatorIndex = meaningfulNodes.indexOf(operator);
+
+      const before = meaningfulNodes[operatorIndex - 1];
+      const after = meaningfulNodes[operatorIndex + 1];
+
+      return { before, operator, after };
+    })
+    .filter(({ before, after }) => {
+      return (
+        (!before || isOperator(before)) && after && isExpressionNode(after)
+      );
+    })
+    .map(({ operator, after: operand }) => {
+      const operatorIndex = nodes.indexOf(operator);
+      const expressionIndex = nodes.indexOf(operand);
+
+      return nodes.slice(operatorIndex, expressionIndex + 1);
+    })
+    .filter((nodes) =>
+      nodes.every(isModifierNodeMember)
+    ) as ModifierNodeMember[][];
+
+  nodeGroups.forEach((members: ModifierNodeMember[]) => {
+    const modifier = createModifier(members);
+    const index = nodes.indexOf(modifier.operator);
+    nodes.splice(index, nodes.length, modifier);
+  });
+
+  return nodes;
+};
---
+++ src/ast/nodes/primitives/node.ts
@@ -0,0 +1,53 @@
+import { isTokenName, TokenTypeName } from "../../tokenize";
+import { GroupType, isGroupType } from "./group";
+
+export type NodeType =
+  | TokenTypeName
+  | GroupType
+  | "fieldReference"
+  | "functionReference";
+
+function isNodeType(type: string): type is NodeType {
+  return (
+    isTokenName(type) ||
+    isGroupType(type) ||
+    type === "fieldReference" ||
+    type === "functionReference"
+  );
+}
+
+export interface Node<T extends NodeType = NodeType> {
+  readonly type: T;
+  readonly start: number;
+  readonly end: number;
+  readonly value: string;
+}
+
+export function isNode(node: unknown): node is Node {
+  if (typeof node !== "object" || node === null) {
+    return false;
+  }
+
+  const { type, start, end, value } = node as Record<string, unknown>;
+  return (
+    typeof type === "string" &&
+    isNodeType(type) &&
+    typeof start === "number" &&
+    typeof end === "number" &&
+    typeof value === "string"
+  );
+}
+
+export type TokenNode<T extends TokenTypeName = TokenTypeName> = Node<T>;
+
+export function isTokenNode(node: Node): node is TokenNode {
+  return isTokenName(node.type);
+}
+
+export function createNodeErrorMessage(
+  node: Node,
+  message: string,
+  type = "Syntax"
+): string {
+  return `${type} Error while parsing ${node.type} node at position ${node.start}: ${message}`;
+}
