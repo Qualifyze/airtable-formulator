a0d933f0e7b132673b8627c7e112a401c6fdca89
Garik Suess <garik.suess@qualifyze.com>
2022-09-18 10:41:01 +0200

    Add export for the parser

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./schema";
 export * from "./compiler";
+export * from "./parse";
 // For backward compatibility (deprecated)
 export * from "./ast/tokenize";
 // Where it should be...
